eaf6168b8cdfa58d5bd31cc7ca7b7fe145207f07
feliot <moligerman@yahoo.com.ar>
2016-06-09 20:36:52 -0300

    Nueva Directiva

---
+++ js/directivas/miDirectiva.js
@@ -0,0 +1,7 @@
+angular.module("ABMangularPHP").directive("utnDirectivaSaludar",function()
+    {
+        return {template:"<h1>Hola UTN Fra</h2>",
+        		restrict:'AEC'
+    			};
+    });
+//obtuvimos un modulo. y definimos nuestra directiva.
